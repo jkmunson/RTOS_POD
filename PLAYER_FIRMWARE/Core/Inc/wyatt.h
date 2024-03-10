6416c1a2bd4d681f44c3e511bd5dba866ed9ab92
WyattGitsGud <wyattl509@gmail.com>
2024-03-10 16:57:36 -0700

    Added a LOT of notes to README, see README for detail on code I uploaded updated wyatt.c updated wyatt.h added header files: ili9341_touch.h, ili9341.h, fonts.h, startup_img.h added C files: ili9341_touch.c, ili9341.c, fonts.c
    Changed nothing in main.c, main.h, or anyone elses files.

--- PLAYER_FIRMWARE/Core/Inc/wyatt.h
+++ PLAYER_FIRMWARE/Core/Inc/wyatt.h
@@ -1,1 +1,52 @@
-void main_wyatt(void);
+#ifndef WYATT_H_
+#define WYATT_H_
+
+// defines
+#define SPACER		12				// vertical spacer - used to tidy display
+#define MAIN_FONT_COLOR	ILI9341_BLACK
+#define BG_COLOR		ILI9341_WHITE
+
+// external variables
+typedef enum {
+	  viewingDirectory, readingFile
+} STATE;
+
+extern STATE currentState;
+extern STATE nextState;
+
+extern char disp_buf[50];		// display buffer - used to print to display
+extern uint16_t sel;			// Used to select specific files
+extern uint16_t tick;			// Handles blinking effect
+extern uint16_t i;				// handles blinking effect
+
+extern bool sel_next;
+extern bool sel_prev;
+extern bool readFile;
+extern bool viewDirectory;
+extern bool contentsPosted;
+
+extern FATFS FatFs; 			//Fatfs handle
+extern FIL fil; 				//File handle
+extern FRESULT fres; 			//Result after operations
+
+extern DWORD free_clusters, free_sectors, total_sectors;	// for SD card stats
+extern FATFS* getFreeFs;									// 		"	"
+
+extern FILINFO fno;				// file info
+extern DIR dir;					// directory
+extern uint16_t nfile; 			// no. of files and directories
+extern char filenames[10][20];	// string array of file names
+
+extern BYTE readBuf[30];
+
+// external functions
+extern void init(void);					// initializes display
+extern void mountFileSystem(void);		// mount file system
+extern void getSDstats(void);			// gets space stats
+extern void showFileNames(void);
+extern void checkNewSelection(void);
+extern void showFileContents(void);
+extern void FSMtest(void);
+
+
+#endif // WYATT_H
--- PLAYER_FIRMWARE/Core/Src/wyatt.c
+++ PLAYER_FIRMWARE/Core/Src/wyatt.c
@@ -1,11 +1,215 @@
+#include "main.h"
+#include "cmsis_os.h"
+#include "fatfs.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+#include <math.h>
+#include "ili9341.h"
+#include "ili9341_touch.h"
+#include "fonts.h"
+
+#include "wyatt.h"
+
+char disp_buf[50];
+uint16_t tick = 0;
+uint16_t i = 0;
+uint16_t sel = 1;
+
+bool sel_next = false;
+bool sel_prev = false;
+bool readFile = false;
+bool viewDirectory = false;
+bool contentsPosted = false;
+
+STATE currentState = viewingDirectory;
+STATE nextState;
+
+//some variables for FatFs
+FATFS FatFs; 	//Fatfs handle
+FIL fil; 		//File handle
+FRESULT fres; //Result after operations
+
+// for statistics from the SD card
+DWORD free_clusters, free_sectors, total_sectors;
+FATFS* getFreeFs;
+
+FILINFO fno;				// file info object
+DIR dir;					// directory object
+uint16_t nfile; 			// no. of files and directories
+char filenames[10][20];		// string array of file names
+
+BYTE readBuf[30];
 
 
-void main_wyatt(void) {
-	//setup
-	// What is the airspeed of an unladen swallow?
+void init() {
+    ILI9341_Unselect();
+    ILI9341_TouchUnselect();
+    ILI9341_Init();
+}
 
-	while(1) {
+void mountFileSystem(){
+	  fres = f_mount(&FatFs, "", 1); //1=mount now
+	  if (fres != FR_OK) {
+		ILI9341_WriteString(0,0,"f_mount error",Font_11x18,ILI9341_BLACK,ILI9341_WHITE);
+		while(1);
+	  }
+}
 
-		vTaskDelay(10); //Delay for 10ms
+void getSDstats(){
+	  fres = f_getfree("", &free_clusters, &getFreeFs);
+	  if (fres != FR_OK) {
+		ILI9341_WriteString(0,0,"f_getfree error",Font_11x18,ILI9341_BLACK,ILI9341_WHITE);
+		while(1);
+	  }
+
+	  //Formula comes from ChaN's documentation
+	  total_sectors = (getFreeFs->n_fatent - 2) * getFreeFs->csize;
+	  free_sectors = free_clusters * getFreeFs->csize;
+
+	  uint32_t ts = total_sectors/2;
+	  uint32_t fs = free_sectors/2;
+	  char ts_str[20];
+	  char fs_str[20];
+	  sprintf(ts_str, "%u", ts);
+	  sprintf(fs_str, "%u", fs);
+
+	  ILI9341_WriteString(0,1*SPACER,"KiB total drive space: ",Font_7x10,MAIN_FONT_COLOR,BG_COLOR);
+	  ILI9341_WriteString(160,1*SPACER,ts_str,Font_7x10,MAIN_FONT_COLOR,BG_COLOR);
+	  ILI9341_WriteString(0,2*SPACER,"KiB available: ",Font_7x10,MAIN_FONT_COLOR,BG_COLOR);
+	  ILI9341_WriteString(160,2*SPACER,fs_str,Font_7x10,MAIN_FONT_COLOR,BG_COLOR);
+}
+
+void gatherFileNames(){
+	  fres = f_opendir(&dir, "/");	// open the root directory ("\" = root)
+	  if (fres != FR_OK){
+			ILI9341_WriteString(0,10*SPACER, "open directory error",Font_7x10,MAIN_FONT_COLOR,BG_COLOR);
+			while(1);
+	  }
+
+	  nfile = 0;
+	  for (;;){
+		  fres = f_readdir(&dir, &fno);					// read a directory item
+		  if (fres != FR_OK || fno.fname[0]==0) break;	// Check for error or end of directory
+
+		  if (fno.fattrib != AM_DIR) {	// ignore directories
+			  strcpy(*(filenames + nfile), fno.fname);
+			  nfile++;
+		  }
+	  }
+
+	  f_closedir(&dir); 		// close directory
+}
+
+void showFileNames(){
+	ILI9341_WriteString(10,2*SPACER, "Directory contents:",Font_7x10,MAIN_FONT_COLOR,BG_COLOR);
+
+	for (i=1; i<nfile; i++){
+		if (i == sel & (tick%2==0)){
+			// flashing effect for selected file
+			ILI9341_WriteString(10,(3+i)*SPACER, filenames[i], Font_7x10,BG_COLOR,MAIN_FONT_COLOR);
+		} else {
+			ILI9341_WriteString(10,(3+i)*SPACER, filenames[i], Font_7x10,MAIN_FONT_COLOR,BG_COLOR);
+		}
 	}
+}
+
+void checkNewSelection(){
+	  if ((sel_next) & (sel < nfile-1) & (currentState == viewingDirectory)){
+		  sel_next = false;
+		  sel++;
+	  }
+	  if ((sel_prev) & (sel > 1) & (currentState == viewingDirectory)){
+		  sel_prev = false;
+		  sel--;
+	  }
+}
+
+void showFileContents(){
+	fres = f_open(&fil, filenames[sel], FA_READ);	// open file
+	if (fres != FR_OK) {
+		while(1);
+	}
+
+	TCHAR* rres = f_gets((TCHAR*)readBuf, 30, &fil);
+	if(rres != 0) {
+		ILI9341_WriteString(10,4*SPACER,readBuf,Font_11x18,MAIN_FONT_COLOR,BG_COLOR);
+	}
+
+	ILI9341_WriteString(10,2*SPACER, "File contents:", Font_7x10,MAIN_FONT_COLOR,BG_COLOR);
+
+	/*
+	for (int i=0; i<10; i++){
+		unsigned char tmp[2];
+		unsigned char bits[2];
+		unsigned char mask = 1;
+		unsigned char char_num[2];
+
+		// ----------- Display character number ---------------
+		char_num[0] = 48 + i;
+		char_num[1] = '\0';
+		ILI9341_WriteString(0,6*10, "char number:", Font_11x18, ILI9341_BLACK, ILI9341_WHITE);
+		ILI9341_WriteString(160,6*10, char_num, Font_11x18, ILI9341_BLACK, ILI9341_WHITE);
+
+		// ---------- Display individual character -----------
+		tmp[0] = readBuf[i];
+		tmp[1] = '\0';
+		ILI9341_WriteString(0,8*10, "character:", Font_11x18, ILI9341_BLACK, ILI9341_WHITE);
+		ILI9341_WriteString(160,8*10, tmp, Font_11x18, ILI9341_BLACK, ILI9341_WHITE);
+
+		// ---------- Display binary representation -----------
+		ILI9341_WriteString(0, 10*10, "binary:", Font_11x18, ILI9341_BLACK, ILI9341_WHITE);
+		for (int j=0; j<8; j++){
+			bits[0] = (tmp[0] & (mask<<j)) != 0;
+			if (bits[0] == 0){
+				bits[0] = 48; // ascii 0
+			} else {
+				bits[0] = 49; // ascii 1
+			}
+			// display binary representation
+			bits[1] = '\0';
+			ILI9341_WriteString(265 - j*15, 10*10, bits, Font_11x18, ILI9341_BLACK, ILI9341_WHITE);
+		}
+	*/
+
+	f_close(&fil);		// close file
+	contentsPosted = true;
+}
+
+void FSMtest(){
+	  /* experiment code */
+	  switch (tick){
+	  case 5:
+		  sel_next = true;
+		  break;
+	  case 10:
+		  readFile = true;
+		  break;
+	  case 15:
+		  viewDirectory = true;
+		  break;
+	  case 20:
+		  sel_next = true;
+		  break;
+	  case 25:
+		  readFile = true;
+		  break;
+	  case 30:
+		  viewDirectory = true;
+		  break;
+	  case 35:
+		  sel_next = true;
+		  break;
+	  case 40:
+		  readFile = true;
+		  break;
+	  case 45:
+		  viewDirectory = true;
+		  break;
+	  default:
+
+	  }
 }
