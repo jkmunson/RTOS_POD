82eacb2494eaa8e246fb6a220445091d3e220090
Jeremy Munson <portalkerrwebsite@gmail.com>
2024-03-17 04:14:56 -0700

    Fix 1 file creation

--- fast_VCP_listen.sh
+++ fast_VCP_listen.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-<<<<<<< HEAD
-gcc baud_set.c -o baud_set 2>1 >/dev/null
-=======
 gcc baud_set.c -o baud_set 2>&1 >/dev/null
->>>>>>> cec6eac (Wrote a program for setting the baud rate on linux to an arbitrary value -> Got the debug UART VCP running at the full 16Mbaud.)
 ./baud_set /dev/ttyACM0 16000000 >/dev/null
 cat /dev/ttyACM0
