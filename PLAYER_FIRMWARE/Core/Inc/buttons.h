ef2bdcf76910bda358dc4073fb5a6a35b5c4dccf
Jeremy Munson <portalkerrwebsite@gmail.com>
2024-03-20 11:25:52 -0700

    Change quotes for <..> on include in buttons.h

--- PLAYER_FIRMWARE/Core/Inc/buttons.h
+++ PLAYER_FIRMWARE/Core/Inc/buttons.h
@@ -1,5 +1,5 @@
 #pragma once
-#include "stdbool.h"
+#include <stdbool.h>
 
 extern volatile bool up_pressed, down_pressed, left_pressed, right_pressed;
 extern volatile bool up_held, down_held, left_held, right_held;
