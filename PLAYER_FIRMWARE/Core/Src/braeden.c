fd0d8c5856d5dfce82c66e8f7ff4d40b82b4a112
Braeden <c1o1l1u1m1b1o@gmail.com>
2024-03-21 14:40:46 -0700

    adc v1

--- PLAYER_FIRMWARE/Core/Src/braeden.c
+++ PLAYER_FIRMWARE/Core/Src/braeden.c
@@ -1,10 +1,22 @@
 #include <FreeRTOS.h>
 #include <task.h>
-
+#include <main.h>
 #include <console.h>
+#include "app_fatfs.h"
 
+uint8_t status;
+FIL adcBuffer;
 void get_audio_sample(void){
-
+	HAL_ADC_Start(&hadc5);
+	status = HAL_ADC_PollForConversion(&hadc5, 1);
+	if(status == HAL_ERROR){
+		Error_Handler();
+	}else if(status == HAL_TIMEOUT){
+		return;
+	}else{
+		HAL_ADC_Stop(&hadc5);
+	}
+	f_write(&adcBuffer, &hadc5, 2, NULL);
 }
 
 void braeden_main(void *ignore __attribute__((unused))) {
