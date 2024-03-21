321b3c66f51675ff1a9f4267c69592667e3ae8de
Braeden <c1o1l1u1m1b1o@gmail.com>
2024-03-21 16:40:32 -0700

    v2

--- PLAYER_FIRMWARE/Core/Src/braeden.c
+++ PLAYER_FIRMWARE/Core/Src/braeden.c
@@ -4,22 +4,26 @@
 #include <console.h>
 #include "app_fatfs.h"
 
-uint8_t status;
-FIL adcBuffer;
+uint8_t entry = 0;
+uint16_t adcBuffer[255];
+FIL microphone;
+FRESULT res;
 void get_audio_sample(void){
+	adcBuffer[entry++] = HAL_ADC_GetValue(&hadc5);
 	HAL_ADC_Start(&hadc5);
-	status = HAL_ADC_PollForConversion(&hadc5, 1);
-	if(status == HAL_ERROR){
-		Error_Handler();
-	}else if(status == HAL_TIMEOUT){
-		return;
-	}else{
-		HAL_ADC_Stop(&hadc5);
+	if (entry == 255){
+		vTaskResume((TaskHandle_t)threads[3]);
 	}
-	f_write(&adcBuffer, &hadc5, 2, NULL);
 }
 
 void braeden_main(void *ignore __attribute__((unused))) {
+	vTaskSuspend(xTaskGetCurrentTaskHandle());
+	res = f_open(&microphone, "recording", FA_WRITE | FA_CREATE_ALWAYS);
+	if (res != FR_OK){
+		while(1);
+	}
+	f_write(&microphone, &adcBuffer, entry, NULL);
+	entry = 0;
 	console_print_time(), console_print("Hi from braeden main\n");
 	console_print_time(), console_printf("Message %d\n", 2);
 	vTaskSuspend(xTaskGetCurrentTaskHandle()); //LEAVE AT THE END
