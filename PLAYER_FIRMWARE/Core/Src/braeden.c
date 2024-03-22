7df3ebe0536f6a62203917e2b52889eb966d49b3
Braeden <c1o1l1u1m1b1o@gmail.com>
2024-03-21 22:32:14 -0700

    update v3

--- PLAYER_FIRMWARE/Core/Src/braeden.c
+++ PLAYER_FIRMWARE/Core/Src/braeden.c
@@ -11,6 +11,7 @@
 uint16_t adcBuffer[BUF_SIZE*2];
 uint16_t *main_buf, *isr_buf;
 
+//Swaps the contents of the isr_buf and main_buf
 void swap_buffers(void){
 	uint16_t *temp = main_buf;
 	main_buf = isr_buf;
@@ -24,9 +25,23 @@ TaskHandle_t task_handle = NULL;
 
 bool aud_buf_ready = false;
 bool ready_to_start = false;
+bool write_complete = false;
+uint32_t num_writes = 0;
 
+//Gets the converted value from the ADC and puts it in a buffer. Repeats until buffer is filled.
 void get_audio_sample(void){
 	static int entry = -1;
+	uint32_t bytes_written;
+
+	if (stop_recording == true & write_complete == true){
+		ready_to_start = false;
+		bytes_written = num_writes * 256 * 2;
+	}
+
+	if (start_recording == true){
+		write_complete = false;
+		ready_to_start = true;
+	}
 
 	if(!ready_to_start) return;
 
@@ -61,6 +76,8 @@ void braeden_main(void *ignore __attribute__((unused))) {
 		//res = f_open(&microphone, "recording", FA_WRITE | FA_CREATE_ALWAYS);
 
 		//f_write(&microphone, &adcBuffer, BUF_SIZE, NULL);
+		write_complete = true;
+		num_writes++;
 	}
 	vTaskSuspend(xTaskGetCurrentTaskHandle()); //LEAVE AT THE END
 	vTaskDelete(NULL);
