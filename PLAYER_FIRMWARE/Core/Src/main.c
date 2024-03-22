60368a01b74cc12b32d21159b63581c0898f789b
Jeremy Munson <portalkerrwebsite@gmail.com>
2024-03-22 02:22:52 -0700

    Timer freq fix

--- PLAYER_FIRMWARE/Core/Src/main.c
+++ PLAYER_FIRMWARE/Core/Src/main.c
@@ -1141,7 +1141,7 @@ static void MX_TIM6_Init(void)
   htim6.Instance = TIM6;
   htim6.Init.Prescaler = 0;
   htim6.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim6.Init.Period = 3627;
+  htim6.Init.Period = 1813;
   htim6.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
   if (HAL_TIM_Base_Init(&htim6) != HAL_OK)
   {
